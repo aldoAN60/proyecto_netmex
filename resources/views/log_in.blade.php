96577e717053b2524353ab40d29e3a73ba10359a
Octavio <octamgo2000@gmail.com>
2023-03-02 19:47:32 -0600

    agrege la rama dev y archivo log_in

---
+++ resources/views/log_in.blade.php
@@ -0,0 +1,43 @@
+<section style="margin-top: 0%;">
+  
+    <div class="card" style="width: 40rem; text-align: left;">
+      <h1 class="escritura">Inicia sesión</h1>
+      <form>
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Email address</label>
+          
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+          <div id="emailHelp" class="form-text">Nunca compartas tu contraseña con nadie</div>
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button  type="" class="bt"><a  style="color:white; text-decoration: none;">Ingresar</a> </button>
+      </form>
+    </div>
+  
+  
+  
+  
+  </section>
+  
+  <section class="redes">
+        <span class="tooltip" >facebook </span>
+        <span><a href="https://www.facebook.com/octavio.martinezgonzalez.737"><i class="fab fa-facebook-f" id="f" ></i></a></span>
+        <span class="tooltip">Twitter</span>
+        <span><i class="fab fa-twitter" id="t" ></i></span>    
+        <span class="tooltip">Instagram</span>
+        <span><i class="fab fa-instagram" id="i"></i></span>   
+        <span class="tooltip">GitHub</span>
+        <span><i class="fab fa-github" id="g"></i></span>    
+        <span class="tooltip">YouTube</span>
+        <span><a href="https://www.youtube.com/@octaviomartinezgonzalez1603/featured"><i class="fab fa-youtube" id="y"></i></a></span>
+    
+    </section>
+    
+  
