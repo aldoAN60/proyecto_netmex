61ef8e34db738df09468ae092e8f6bd18087601a
aldo <aldoan60@gmail.com>
2023-03-03 01:33:54 -0600

    se cambio un r por una R XD

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','inicio')->name('inicio');
 Route::view('/sign_up','sign_up')->name('sign_up');
-route::view('/paypal-donation','paypal-donation')->name('paypal-donation');
+Route::view('/paypal-donation','paypal-donation')->name('paypal-donation');
